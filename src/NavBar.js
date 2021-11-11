25071124e7d22c005f8fe3854e24cbe37034338c
AntonioEsp <github email address>
2021-11-10 21:39:08 -0600

    updated car brands

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,9 +4,9 @@ import {Link, withRouter} from "react-router-dom"
 const NavBar = () => {
     return (
         <React.Fragment>
-            <Link to="/">Home Page</Link>&nbsp; &nbsp; &nbsp;
-            <Link to="/car_brands">Car Brands</Link>&nbsp; &nbsp; &nbsp;
-            <Link to="/cars">Cars</Link>&nbsp; &nbsp; &nbsp;
+            <Link to="/">Home Page</Link>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+            <Link to="/car_brands">Car Brands</Link>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+            <Link to="/cars">Cars</Link>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             <br></br>
         </React.Fragment>
     )
